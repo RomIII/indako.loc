fbcb06447a4dea652e79f3d8c5364f3061e1699e
Rom <1633131@mail.ru>
2018-02-14 09:19:37 +0300

    дизайн1

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ require($_SERVER['DOCUMENT_ROOT'] . '/bitrix/header.php');
 $APPLICATION->SetTitle("Главная");
 ?>
 
-12345677788666000
+<b>1234567778866</b>6000
 
 <?
 require($_SERVER['DOCUMENT_ROOT'] . '/bitrix/footer.php');
